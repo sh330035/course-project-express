132b83894b568f31c6ca0900aacddb417ea7544b
david3335 <sh330035@gmail.com>
2021-12-22 09:54:30 +0800

    feat. "instructor get course api"

--- routes/course-route.js
+++ routes/course-route.js
@@ -20,6 +20,18 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/instructor/:_instructor_id", (req, res) => {
+  let { _instructor_id } = req.params;
+  Course.find({ instructor: _instructor_id })
+    .populate("instructor", ["username", "email"])
+    .then((data) => {
+      res.send(data);
+    })
+    .catch(() => {
+      res.status(500).send("Cannot get course.");
+    });
+});
+
 router.get("/:_id", (req, res) => {
   let { _id } = req.params;
   Course.findOne({ _id })
